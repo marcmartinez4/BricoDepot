8092fab7ab2c0cc4bdc9d9ed4bc1aead5cc1db0a
Marc <marcmartinezsofp@bernatelferrer.cat>
2024-01-29 17:26:23 +0100

    Mostrar numero pedido en la página de info, QR no da la ruta correcta.

--- src/informacionPedido.js
+++
@@ -1,4 +0,0 @@
-let url = new URLSearchParams(window.location.search);
-let pedido_id = url.get("pedido_id");
-
-fetch
--- vista/QR_pedido.php
+++ vista/QR_pedido.php
@@ -15,7 +15,10 @@
                     <div class="col-sm-12 col-md-12 col-lg-12">
                         <h1 class="titulo">TU PEDIDO</h1>
                         <hr>
-                            
+                        <input type="hidden" id="pedido_id" value="<?= strval($_SESSION['pedido_id']) ?>">
+                        
+                        
+
                         <div class="col-sm-12 col-md-12 col-lg-12">
                             <div class="d-flex justify-content-center">
                                 <div class="row d-flex justify-content-center">
@@ -27,15 +30,12 @@
                                 </div>
                             </div>
                         </div>
-
-                        
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
-    <script src="src/añadirReseña.js"></script>
     <script src="src/QR.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
